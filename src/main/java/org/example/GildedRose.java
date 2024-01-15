837dead41776fef232f1542819b1fca9afcbf95a
wdc <william.declercq@gmail.com>
2024-01-15 18:58:38 +0100

    Woops..

--- src/main/java/org/example/GildedRose.java
+++ src/main/java/org/example/GildedRose.java
@@ -1,2 +1,143 @@
-package org.example;public class GildedRose {
+package org.example;
+
+public class GildedRose {
+    Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for(Item item : items){
+            handleAgedBrie(item);
+            handleBackstagePass(item);
+            handleConjured(item);
+            handleOther(item);
+        }
+    }
+
+    //--------------------------------------------------------------------------------
+    //          HANDLERS
+    //--------------------------------------------------------------------------------
+
+    /**
+     * Handles Aged Brie
+     * "Aged Brie" actually increases in Quality the older it gets
+     * @param item inventory item
+     */
+    private void handleAgedBrie(Item item) {
+        boolean isAgedBrie = item.name.contains("Aged Brie");
+        boolean isExpired = item.sellIn < 0;
+        //if aged brie increase quality
+        if(isAgedBrie){
+            increaseQuality(item);
+            decreaseSellIn(item);
+        }
+        //if aged brie and expired increase quality once more
+        if(isAgedBrie && isExpired){
+            increaseQuality(item);
+        }
+    }
+
+    /**
+     * Handles Conjured
+     * "Conjured" items degrade in Quality twice as fast as normal items
+     * @param item inventory item
+     */
+    private void handleConjured(Item item) {
+        boolean isConjured = item.name.contains("Conjured");
+        boolean isExpired = item.sellIn < 0;
+        //if conjured decrease quality by 2
+        if(isConjured){
+            decreaseQuality(item, 2);
+            decreaseSellIn(item);
+        }
+        //if conjured and expired decrease quality by 4
+        if(isConjured && isExpired){
+            decreaseQuality(item, 2);
+        }
+    }
+
+    /**
+     * Handles Backstage Pass
+     * "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+     * Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+     * Quality drops to 0 after the concert
+     * @param item inventory item
+     */
+    private void handleBackstagePass(Item item) {
+        boolean isBackstagePass = item.name.contains("Backstage passes");
+        boolean isExpired = item.sellIn < 0;
+        //if backstage pass
+        if(isBackstagePass){
+            increaseQuality(item);
+            if(item.sellIn <= 10){
+                increaseQuality(item);
+            }
+            if(item.sellIn <= 5){
+                increaseQuality(item);
+            }
+            decreaseSellIn(item);
+        }
+        //if backstage pass and expired set quality to 0
+        if(isBackstagePass && isExpired){
+            item.quality = 0;
+        }
+    }
+
+    /**
+     * Handles Other
+     * @param item inventory item
+     */
+    private void handleOther(Item item) {
+        boolean isAgedBrie = item.name.contains("Aged Brie");
+        boolean isBackstagePass = item.name.contains("Backstage passes");
+        boolean isSulfuras = item.name.contains("Sulfuras");
+        boolean isConjured = item.name.contains("Conjured");
+        boolean isExpired = item.sellIn < 0;
+        //if not aged brie, backstage pass, or sulfuras decrease quality
+        if(!isAgedBrie && !isBackstagePass && !isSulfuras && !isConjured){
+            decreaseQuality(item, 1);
+            decreaseSellIn(item);
+        }
+        //if not aged brie, backstage pass, or sulfuras and expired decrease quality by 2
+        if(!isAgedBrie && !isBackstagePass && !isSulfuras && !isConjured && isExpired){
+            decreaseQuality(item, 1);
+        }
+    }
+
+    //--------------------------------------------------------------------------------
+    //          HELPERS
+    //--------------------------------------------------------------------------------
+
+    /**
+     * Increases quality by 1
+     * @param item inventory item
+     */
+    private void increaseQuality(Item item){
+        if(item.quality < 50){
+            item.quality++;
+        }
+    }
+
+    /**
+     * Decreases quality by amout
+     * @param item inventory item
+     * @param amount amount to decrease quality by
+     */
+    private void decreaseQuality(Item item, int amount){
+        if(item.quality > 0){
+            item.quality-=amount;
+        }
+    }
+
+    /**
+     * Decreases sellIn by 1
+     * @param item inventory item
+     */
+    private void decreaseSellIn(Item item) {
+        item.sellIn--;
+    }
+
+
 }
--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -1,2 +1,192 @@
-package PACKAGE_NAME;public class GildedRoseTEst {
+import org.example.GildedRose;
+import org.example.Item;
+import org.junit.jupiter.api.Test;
+
+public class GildedRoseTest {
+
+    @Test
+    void testBrieOneDayPasses(){
+        Item item1 = new Item("Aged Brie", 2, 8);
+        Item[] items = new Item[] { item1 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 9;
+        assert items[0].sellIn == 1;
+    }
+
+    @Test
+    void testBrieExpired(){
+        Item item1 = new Item("Aged Brie", -1, 8);
+        Item[] items = new Item[] { item1 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 10;
+        assert items[0].sellIn == -2;
+    }
+
+    @Test
+    void testBrieMaxQuality(){
+        Item item1 = new Item("Aged Brie", 2, 50);
+        Item[] items = new Item[] { item1 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 50;
+        assert items[0].sellIn == 1;
+    }
+
+    @Test
+    void testBackstagePassesTenOrLessDays(){
+        Item item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 8);
+        Item[] items = new Item[] { item1 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 10;
+        assert items[0].sellIn == 9;
+    }
+
+    @Test
+    void testBackstagePassesFiveOrLessDays(){
+        Item item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 8);
+        Item[] items = new Item[] { item1 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 11;
+        assert items[0].sellIn == 4;
+    }
+
+    @Test
+    void testBackstagePassesExpired(){
+        Item item1 = new Item("Backstage passes to a TAFKAL80ETC concert", -1, 8);
+        Item[] items = new Item[] { item1 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 0;
+        assert items[0].sellIn == -2;
+    }
+
+    @Test
+    void testBackstagePassesMaxQuality(){
+        Item item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50);
+        Item[] items = new Item[] { item1 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 50;
+        assert items[0].sellIn == 4;
+    }
+
+    @Test
+    void testBackstagePassesMoreThan10Days(){
+        Item item1 = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 8);
+        Item[] items = new Item[] { item1 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 9;
+        assert items[0].sellIn == 10;
+    }
+
+    @Test
+    void testSulfuras(){
+        Item item1 = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+        Item item2 = new Item("Sulfuras, Hand of Ragnaros", -1, 80);
+        Item[] items = new Item[] { item1, item2 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 80;
+        assert items[0].sellIn == 0;
+        assert items[1].quality == 80;
+        assert items[1].sellIn == -1;
+    }
+
+    @Test
+    void testConjuredOneDayPasses(){
+        Item item1 = new Item("Conjured Mana Cake", 2, 8);
+        Item[] items = new Item[] { item1 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 6;
+        assert items[0].sellIn == 1;
+    }
+
+    @Test
+    void testConjuredExpired(){
+        Item item1 = new Item("Conjured Mana Cake", -1, 8);
+        Item[] items = new Item[] { item1 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 4;
+        assert items[0].sellIn == -2;
+    }
+
+    @Test
+    void testOtherOneDayPasses(){
+        Item item1 = new Item("Other", 2, 8);
+        Item item2 = new Item("Other", 2, 0);
+        Item item3 = new Item("Other", 2, 1);
+        Item item4 = new Item("Other", 2, 50);
+
+        Item[] items = new Item[] { item1, item2, item3, item4 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 7;
+        assert items[0].sellIn == 1;
+        assert items[1].quality == 0;
+        assert items[1].sellIn == 1;
+        assert items[2].quality == 0;
+        assert items[2].sellIn == 1;
+        assert items[3].quality == 49;
+        assert items[3].sellIn == 1;
+    }
+
+    @Test
+    void testOtherOneDayPassesExpired(){
+        Item item1 = new Item("Other", -1, 8);
+        Item item2 = new Item("Other", -1, 0);
+        Item item3 = new Item("Other", -1, 1);
+        Item item4 = new Item("Other", -1, 50);
+
+        Item[] items = new Item[] { item1, item2, item3, item4 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 6;
+        assert items[0].sellIn == -2;
+        assert items[1].quality == 0;
+        assert items[1].sellIn == -2;
+        assert items[2].quality == 0;
+        assert items[2].sellIn == -2;
+        assert items[3].quality == 48;
+        assert items[3].sellIn == -2;
+    }
+
+    @Test
+    void mixedItemsTest() {
+        Item item1 = new Item("Aged Brie", 2, 8);
+        Item item2 = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+        Item item3 = new Item("Sulfuras, Hand of Ragnaros", 0, 80);
+        Item item4 = new Item("Sulfuras, Hand of Ragnaros", -1, 80);
+        Item item5 = new Item("Conjured Mana Cake", 2, 8);
+        Item item6 = new Item("Conjured Mana Cake", -1, 8);
+        Item item7 = new Item("Other", 2, 8);
+        Item item8 = new Item("Other", -1, 8);
+
+        Item[] items = new Item[] { item1, item2, item3, item4, item5, item6, item7, item8 };
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        assert items[0].quality == 9;
+        assert items[0].sellIn == 1;
+        assert items[1].quality == 21;
+        assert items[1].sellIn == 14;
+        assert items[2].quality == 80;
+        assert items[2].sellIn == 0;
+        assert items[3].quality == 80;
+        assert items[3].sellIn == -1;
+        assert items[4].quality == 6;
+        assert items[4].sellIn == 1;
+        assert items[5].quality == 4;
+        assert items[5].sellIn == -2;
+        assert items[6].quality == 7;
+        assert items[6].sellIn == 1;
+        assert items[7].quality == 6;
+        assert items[7].sellIn == -2;
+    }
+
 }
